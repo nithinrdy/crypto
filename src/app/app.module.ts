ae11144e80e53a3c1f7e77fbf9f3616d52f0058f
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-05-13 02:08:07 +0530

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,9 @@ import { AppComponent } from './app.component';
 import { BaseComponent } from './components/base/base.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BaseComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, BaseComponent],
+	imports: [BrowserModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
