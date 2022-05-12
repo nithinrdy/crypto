25878b8338873d17ea73ffeeee0e29d962b266c1
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-05-13 02:01:34 +0530

    set-up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BaseComponent } from './components/base/base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent
   ],
   imports: [
     BrowserModule
