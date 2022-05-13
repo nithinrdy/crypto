f293374c0227467c2442e8af0fd6e35b4eb252d9
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-05-13 14:21:35 +0530

    revert base component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BaseComponent } from './components/base/base.component';
-
 @NgModule({
-	declarations: [AppComponent, BaseComponent],
+	declarations: [AppComponent],
 	imports: [BrowserModule],
 	providers: [],
 	bootstrap: [AppComponent],
