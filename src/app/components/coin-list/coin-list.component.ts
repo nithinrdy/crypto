62d75acd0b92b342c1d2bc16029c3a92557c1fd8
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-05-22 11:20:18 +0530

    add coin-list component

---
+++ src/app/components/coin-list/coin-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'coin-list',
+  templateUrl: './coin-list.component.html',
+  styleUrls: ['./coin-list.component.css']
+})
+export class CoinListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
