9d72859a46d2b0f2de8c9dfc039397b7d1ee5210
Nithin <78612244+nithinrdy@users.noreply.github.com>
2022-05-25 18:03:32 +0530

    retrieve coin symbol from url

--- src/app/components/coin-detail/coin-detail.component.ts
+++ src/app/components/coin-detail/coin-detail.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { map } from 'rxjs';
+import { tap } from 'rxjs';
 
 @Component({
-  selector: 'app-coin-detail',
-  templateUrl: './coin-detail.component.html',
-  styleUrls: ['./coin-detail.component.css']
+	selector: 'app-coin-detail',
+	templateUrl: './coin-detail.component.html',
+	styleUrls: ['./coin-detail.component.css'],
 })
 export class CoinDetailComponent implements OnInit {
+	constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
+	coinSymbol!: string;
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.route.paramMap
+			.pipe(
+				map((params) => params.get('id')),
+				tap((id) =>
+					id
+						? (this.coinSymbol = id)
+						: (this.coinSymbol = 'undefined')
+				)
+			)
+			.subscribe((id) => {});
+	}
 }
